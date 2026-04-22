d63d2f2678dd36763f37e69b6c6823ee6a4b7ae2
Jefferson Costa Dias <jeffersoncosta2@gmail.com>
2026-04-22 07:02:04 -0300

    Ajuste BaseEnum

--- src/enums/BaseEnum.php
+++ src/enums/BaseEnum.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Websitesa\Yii2\Helpers\Enums;
 
-enum BaseEnum: string
+enum BaseEnum: int
 {
-    case Active = 'Ativo';
-    case Inactive = 'Inativo';
+    case Active = 1;
+    case Inactive = 2;
 
     public function label(string $lang = 'pt'): string
     {
